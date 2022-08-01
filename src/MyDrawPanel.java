9e0cbfb60b4f3e12d65632948012efbd2b2fd908
irah rosete <irah.rosete@outlook.com>
2022-08-02 00:06:27 +1000

    update MyDrawPanel and implement button click action listener

--- src/MyDrawPanel.java
+++ src/MyDrawPanel.java
@@ -4,23 +4,15 @@ import java.util.Random;
 
 public class MyDrawPanel extends JPanel {
     public void paintComponent(Graphics g) {
+        g.setColor(Color.DARK_GRAY);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
-
         Random random = new Random();
         int red = random.nextInt(256);
         int green = random.nextInt(256);
         int blue = random.nextInt(256);
 
         Color randomColor = new Color(red, green, blue);
-        g.setColor(Color.orange);
+        g.setColor(randomColor);
         g.fillOval(100, 75, 100, 100);
     }
-
-//    public static void main(String[] args) {
-//        JFrame frame = new JFrame();
-//        frame.getContentPane().add(new MyDrawPanel());
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(300, 300);
-//        frame.setVisible(true);
-//    }
 }
--- src/SimpleGui2.java
+++ src/SimpleGui2.java
@@ -2,9 +2,32 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static java.awt.BorderLayout.*;
+
 public class SimpleGui2 implements ActionListener {
     private JFrame frame;
+    private JButton button;
+    private MyDrawPanel panel;
 
+    public static void main(String[] args) {
+        SimpleGui2 gui = new SimpleGui2();
+        gui.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button = new JButton("change colours");
+        button.addActionListener(this);
+
+        panel = new MyDrawPanel();
+
+        frame.getContentPane().add(SOUTH, button);
+        frame.getContentPane().add(CENTER, panel);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         frame.repaint();
