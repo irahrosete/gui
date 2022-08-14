5de8e3916774eb089f9b04bc2a7f0449abbeb208
irah rosete <irah.rosete@outlook.com>
2022-08-14 12:34:10 +1000

    create animation

---
+++ src/SimpleAnimation.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+public class SimpleAnimation {
+    private int xPos = 70;
+    private int yPos = 70;
+
+    public static void main(String[] args) {
+        SimpleAnimation gui = new SimpleAnimation();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        DrawPanel panel = new DrawPanel();
+
+        frame.getContentPane().add(panel);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+
+        for (int i = 0; i < 130; i++) {
+            xPos++;
+            yPos++;
+            panel.repaint();
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(50);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class DrawPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            g.setColor(Color.GREEN);
+            g.fillOval(xPos, yPos, 40, 40);
+        }
+    }
+}
