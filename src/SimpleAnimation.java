f449dddffc72fc1cec08f7022bcb96e0a322e032
irah rosete <irah.rosete@outlook.com>
2022-08-14 12:38:45 +1000

    update panel for animation

--- src/SimpleAnimation.java
+++ src/SimpleAnimation.java
@@ -36,7 +36,9 @@ public class SimpleAnimation {
 
     class DrawPanel extends JPanel {
         public void paintComponent(Graphics g) {
-            g.setColor(Color.GREEN);
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(0,0, this.getWidth(), this.getHeight());
+            g.setColor(Color.PINK);
             g.fillOval(xPos, yPos, 40, 40);
         }
     }
